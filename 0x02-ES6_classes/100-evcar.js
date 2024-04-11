42426de9bcd88e6a344a30bcc4bd6e5a9f5c501b
Jassocc <clnearle@gmail.com>
2024-04-11 15:11:44 +0000

    task 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
+}
+
+export default EVCar;
