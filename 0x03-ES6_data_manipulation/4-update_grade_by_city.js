4f648e12e25f8641d1eb12aca7b862e18c153c60
Jassocc <clnearle@gmail.com>
2024-04-15 14:22:47 +0000

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (matchingGrade) {
+        return { ...student, grade: matchingGrade.grade };
+      }
+      return { ...student, grade: 'N/A' };
+    });
+}
+export default updateStudentGradeByCity;
