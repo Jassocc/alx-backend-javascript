fe09e9ec81978c052c5eee9e01e443ec5397cff2
Jassocc <clnearle@gmail.com>
2024-06-03 14:16:45 +0000

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+// creates a http server
+const http = require('http');
+const host = 'localhost';
+const port = 1245;
+const app = http.createServer((request, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+app.listen(port, host, () => {
+});
+module.exports = app;
