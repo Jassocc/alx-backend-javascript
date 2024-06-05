34125043b88011da1e712b74689aa23950466b7c
Jassocc <clnearle@gmail.com>
2024-06-05 18:31:13 +0000

    task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,3 @@
 // accepts 2 numbers and gives sum
-const calculateNumber = (a, b) => {
-  return Math.round(a) + Math.round(b);
-}
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 module.exports = calculateNumber;
