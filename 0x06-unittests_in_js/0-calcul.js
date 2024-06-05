27d00ff4aa2dfd67ddb7fefaedf588eb679829d5
Jassocc <clnearle@gmail.com>
2024-06-05 08:13:51 +0000

    task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+// accepts 2 numbers and gives sum
+const calculateNumber = (a, b) => {
+  return Math.round(a) + Math.round(b);
+}
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+// tests calculate number test
+const assert = require('assert');
+const calculateNumber = require("./0-calcul");
+
+describe('calculateNumber', () => {
+  it('check the rounded part', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.2), 4);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('negative num', () => {
+    assert.strictEqual(calculateNumber(-1, 3), 2);
+    assert.strictEqual(calculateNumber(1, -3.7), -3);
+    assert.strictEqual(calculateNumber(1, -3.2), -2);
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+  });
+});
